a496ee3fd1ff6017385c03ece7b6cbcd5a7bb98a
Austin Lin <austin@austinylin.com>
2015-09-20 15:25:47 -0400

    bonfire/everything-be-true

---
+++ bonfire/everything-be-true.js
@@ -0,0 +1,5 @@
+function every(collection, pre) {
+  return collection.some(e => e[pre]);
+}
+
+every([{'user': 'Tinky-Winky', 'sex': 'male'}, {'user': 'Dipsy', 'sex': 'male'}, {'user': 'Laa-Laa', 'sex': 'female'}, {'user': 'Po', 'sex': 'female'}], 'sex');
